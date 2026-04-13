03ee15040eff95f359fb3d663d51bdf38fd861bb
Manish <manishkumar9540898@gmail.com>
2026-04-13 19:20:19 +0530

    chore: setup nativewind & prettier

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex bg-black text-7xl text-cyan-300">
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
